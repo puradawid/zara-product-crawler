f55d3fa312bed38a8a10638571002ec7a2d693d9
Dawid Pura <puradawid@gmail.com>
2016-08-26 20:00:40 +0200

    Fixed code to being reused somewhere else

--- crawl-product-size.py
+++ crawl-product-size.py
@@ -1,3 +1,7 @@
+# ZARA PRODUCT SIZE CRAWLER - reads zara page and finds sizes available (or tests against given)
+#
+# Who cares about license? Do whatever you want with this code.
+
 import os;
 import urllib2;
 import sys;
@@ -6,33 +10,44 @@ import lxml.cssselect
 
 USER_AGENT_FAKE = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36'
 
-if os.name == 'linux':
-    import pynotify
+# Simple class toa wrap up reading functionallity of reading HTML page using HTTP
+class DOMPageContent:
+    _url = None
 
-if os.name == 'windows':
-    import baloontip
+    def __init__(self, url):
+        self._url = url;
 
-def load_page(url): 
-    request = urllib2.Request(url)
-    request.add_header('User-Agent', USER_AGENT_FAKE)
-    return urllib2.urlopen(request).read().decode("iso-8859-2");
+    def read(self):
+        request = urllib2.Request(self._url);
+        request.add_header('User-Agent', USER_AGENT_FAKE)
+        return html.fromstring(urllib2.urlopen(request).read().decode("iso-8859-2"))
 
-def parse_to_tree(markup):
-    return html.fromstring(markup)
+# Wrapper class for a data storage and read - fetch all HTML page and looks for size nodes
+# maps text from nodes directly
+class ZaraProduct:
+    _sizes = None
 
-def find_sizes(root_element):
-    return map(lambda element : element.text, root_element.cssselect('tr.product-size:not(.disabled) td.size-name'))
+    def __init__(self, url):
+        self._page_content = DOMPageContent(url)
 
-def does_have_size(url, size):
-    return size in find_sizes(parse_to_tree(load_page(url)))
+    def sizes(self):
+        if self._sizes is None:
+            self._sizes = self._page_content.read().cssselect('tr.product-size:not(.disabled) td.size-name')
+            self._sizes = map(lambda e : e.text, self._sizes);
+        return self._sizes;
 
+    def have_size(self, size):
+        return size in self.sizes()
+
+# Command line handling
 if len(sys.argv) >= 2 and sys.argv[1] == 'test':
     test_url = 'http://www.zara.com/pl/pl/kobieta/spodnie/zobacz-wi%C4%99cej/%C5%BCakardowe-spodnie-typu-culotte-c733898p3956074.html'
-    assert len(load_page(test_url)) > 0
-    assert parse_to_tree(load_page(test_url)) is not None
-    assert find_sizes(parse_to_tree(load_page(test_url))) is not None
+    product = ZaraProduct(test_url)
+    assert product.sizes() is not None
 elif len(sys.argv) >= 3 and sys.argv[2]:
     desired_size = sys.argv[2]
-    print does_have_size(sys.argv[1], sys.argv[2])
+    product = ZaraProduct(sys.argv[1]);
+    print product.have_size(sys.argv[2]);
 else:
-    print find_sizes(parse_to_tree(load_page(sys.argv[1])))
+    product = ZaraProduct(sys.argv[1]);
+    print product.sizes();
