0f27db0d2a30ac2fcbf1f0a4ab5160761cc7a7b8
Dawid Pura <puradawid@gmail.com>
2016-09-03 08:20:33 +0200

    Added timer instead of thread instance for background-notifier

--- background-notifier.py
+++ background-notifier.py
@@ -4,14 +4,17 @@ import threading, time
 
 # Notification to make a notify call (probably depends on System used
 class NotificationHandler:
+
     def notify(self, message):
         raise Exception("Notification not implemented - probably generic class")
 
 class PrintLine(NotificationHandler):
+
     def notify(self, message):
         print message + '\n'
 
 class LinuxNotify(NotificationHandler):
+
     def notify(self, message):
         import subprocess as sub
         sub.Popen(['notify-send', '-t', '2', message])
@@ -20,36 +23,44 @@ class Background:
 
     def __init__(self, predicate, sleep=10, notification_handler = PrintLine()):
         self._stop_event = threading.Event()
-        self._background_thread = threading.Thread(target=self._loop, args=(self._stop_event,))
         self._predicate = predicate
         self._sleep = sleep
         self._notification_handler = notification_handler
 
     def start(self):
-        self._background_thread.start()
+        self._loop(self._stop_event)
 
     def join(self):
         self._background_thread.join()
 
     def stop(self):
         self._stop_event.set()
+        if self._current_timer is not None:
+            self._current_timer.cancel()
 
     def _notify(self, msg):
         self._notification_handler.notify(msg)
 
     def _loop(self, stop):
-        while not stop.is_set():
-            msg = self._predicate()
-            if msg is not None:
-                self._notify(msg)
-            time.sleep(self._sleep)
-
+        print 'loop\n'
+        if stop.is_set(): 
+            return
+        msg = self._predicate()
+        if msg is not None:
+            self._notify(msg)
+        self._current_timer = threading.Timer(self._sleep, self._loop, (stop,)).start()
+counter = 1
 # Testing
-def true():
-    return 'HYHYHY'
+def test():
+    def true():
+        global counter
+        counter += 1
+        return 'Test method ' + str(counter)
 
-notifier = Background(predicate=true, notification_handler=LinuxNotify())
-notifier.start()
-input('Whatever')
-notifier.stop()
+    notifier = Background(predicate=true, notification_handler=LinuxNotify())
+    notifier.start()
+    raw_input('Click something to stop the worker')
+    print "Stop signal sent"
+    notifier.stop()
 
+test()
